c9c0a706a62c0f1c3fc40b51e7a49d3195a10240
Pierre <pierrelouis.abraham@gmail.com>
2023-01-04 10:59:02 +0100

    first push close modal + check modal

--- modal.js
+++ modal.js
@@ -19,3 +19,30 @@ function editNav() {
   function launchModal() {
     modalbg.style.display = "block";
   }
+
+  function closeModal() {
+    modalbg.style.display = "none";
+  }
+
+  function validate() {
+    let bool = true;
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(document.getElementById("first").value.length < 2) {
+      document.getElementById("first").style.borderColor = "red";
+      alert("Saisie invalide");
+      return false;
+    }
+    if(document.getElementById("last").value.length < 2) {
+      document.getElementById("last").style.borderColor = "red";
+      alert("Saisie invalide");
+      return false;
+    }
+    if(document.getElementById("email").value.match(regex)) {
+      return true;
+    } else {
+      alert("Saisie mail invalide");
+      document.getElementById("email").style.borderColor = "red";
+      return false;
+    }
+    
+  }
