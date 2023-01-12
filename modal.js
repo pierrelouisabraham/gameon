a4662a5409faa68f7d54adfa22e957b285ef86d4
Pierre <pierrelouis.abraham@gmail.com>
2023-01-12 09:35:04 +0100

    check validation form

--- modal.js
+++ modal.js
@@ -11,11 +11,15 @@ function editNav() {
   const modalbg = document.querySelector(".bground");
   const modalBtn = document.querySelectorAll(".modal-btn");
   const formData = document.querySelectorAll(".formData");
-  /* const form = document.querySelector("form").addEventListener("submit", evt => {
-    evt.preventDefault
-    
-    
-  }) */
+  const firstNameInput = document.getElementById("first").value;
+  var form = document.querySelector("form").addEventListener("submit", evt => {
+    let error = false;
+    error = checkFirstname(evt) || checkName(evt) || checkEmail(evt) || checkBirthdate(evt) || checkRadio(evt) || checkTerms(evt);
+    if (error)
+      evt.preventDefault();
+    })
+  
+  
   // launch modal event
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
   
@@ -25,13 +29,88 @@ function editNav() {
   }
 
   function closeModal() {
-
     modalbg.style.display = "none";
   }
 
-  function validate() {
-    if (document.reserve.first.value == "toto")
-      return false
-    else 
-      return true
-  } 
+  function hideElement(el) {
+    el.style.display = "none";
+  }
+
+  function showElement(el) {
+    el.style.display = "inline";
+  }
+
+  function checkFirstname(evt) {
+    console.log(evt);
+    console.log(formData)
+    if (evt["path"][0][0].value.length < 2) {
+      /* showElement() */
+      evt["path"][0][0].dataset.errorVisible = "true";
+      formData[0].dataset.errorVisible = "true";
+      console.log(evt);
+      console.log(formData, 2)
+      return true;
+    }
+    evt["path"][0][0].dataset.errorVisible = "false";
+    formData[0].dataset.errorVisible = "false";
+    console.log(evt);
+    return false;
+  }
+
+  function checkName(evt) {
+    if (evt["path"][0][1].value.length < 2) {
+      evt["path"][0][1].dataset.errorVisible = "true";
+      formData[1].dataset.errorVisible = "true";
+      return true;
+    }
+    evt["path"][0][1].dataset.errorVisible = "false";
+    formData[1].dataset.errorVisible = "false";
+    return false;
+  }
+
+  function checkEmail(evt) {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!evt["path"][0][2].value.match(regex)) {
+      evt["path"][0][2].dataset.errorVisible = "true";
+      formData[2].dataset.errorVisible = "true";
+      return true;
+    }
+    evt["path"][0][2].dataset.errorVisible = "false";
+    formData[2].dataset.errorVisible = "false";
+    return false;
+  }
+
+  function checkBirthdate(evt) {
+    if (evt["path"][0][3].value == "") {
+      evt["path"][0][3].dataset.errorVisible = "true";
+      formData[3].dataset.errorVisible = "true";
+      return true;
+    }
+    evt["path"][0][3].dataset.errorVisible = "false";
+    formData[3].dataset.errorVisible = "false";
+    return false;
+  }
+
+  function checkRadio(evt) {
+    for(let i = 5; i < evt["path"][0].length - 3; i++){
+      console.log(i)
+      if (evt["path"][0][i].checked) {
+        formData[i].dataset.errorVisible = "false";
+        return false;
+      }
+    }
+      formData[5].dataset.errorVisible = "true";
+      return true;
+  }
+
+  function checkTerms(evt) {
+    if (!evt["path"][0][11].checked) {
+      console.log(salut)
+      formData[6].dataset.errorVisible = "true";
+      return false;
+      
+    }
+    console.log(salut, 2)
+    formData[6].dataset.errorVisible = "false";
+    return true;
+  }
